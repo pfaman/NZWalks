5ae8738bd708d9652957b94af1d9b486ed847124
Aman Kant <amantheiit@gmail.com>
2026-02-28 12:30:13 +0530

    Image Code Updated

--- NZWalks/NZWalks.API/obj/Debug/net9.0/NZWalks.API.AssemblyInfo.cs
+++ NZWalks/NZWalks.API/obj/Debug/net9.0/NZWalks.API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("NZWalks.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6feba70c82e43e253fcb48b9840242c585f40259")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+31b55550a15242691add133a9e8fc7498a5f8e6c")]
 [assembly: System.Reflection.AssemblyProductAttribute("NZWalks.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("NZWalks.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
