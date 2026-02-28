20065cfe74e9785085d79d1a8492ce11a7859036
Aman Kant <amantheiit@gmail.com>
2026-02-28 11:23:35 +0530

    Test CI/CD

--- NZWalks/NZWalks.API/Models/DTO/DifficultyDto.cs
+++ NZWalks/NZWalks.API/Models/DTO/DifficultyDto.cs
@@ -2,7 +2,7 @@ namespace NZWalks.API.Models.DTO
 {
     public class DifficultyDto
     {
-        public Guid Id { get; set; }
+        public Guid Id { get; set; } // Id
         public string? Name { get; set; }
     }
 }
